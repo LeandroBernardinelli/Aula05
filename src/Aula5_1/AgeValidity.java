64b48e647d18eb7892fb8b9f01237db1b3debd1e
ALUNO <ALUNO@LAB04-PC11>
2024-06-18 21:43:35 -0300

    Aula5_1

---
+++ src/Aula5_1/AgeValidity.java
@@ -0,0 +1,19 @@
+package Aula5_1;
+
+import java.util.Scanner;
+
+public class AgeValidity {
+
+    public static void main(String[] args) {
+Scanner scanner = new Scanner(System.in);        
+
+System.out.println("informe sua idade:");
+String idade = scanner.nextLine();
+boolean drivingUnderAge; 
+drivingUnderAge = false;
+        
+
+
+       System.out.println(drivingUnderAge);
+    }
+}
