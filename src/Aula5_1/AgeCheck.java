4f495aa88ca6d7e9916dfc1d506f07bb4d83a3cf
ALUNO <ALUNO@LAB04-PC11>
2024-06-19 21:39:26 -0300

    Aula5_1

---
+++ src/Aula5_1/AgeCheck.java
@@ -0,0 +1,15 @@
+public class AgeCheck {
+   
+    public static void main(String[] args) {
+        int myAge = 19;   // I am 19; let me see if I can drive yet
+        
+        if (myAge >= 16) {
+            System.out.println("Tenho idade suficiente para ter carteira de motorista!");
+        }
+        if (myAge <= 16) {
+            System.out.println("Ainda não tenho idade suficiente... :*(");
+        }
+    }
+}
+    
+
